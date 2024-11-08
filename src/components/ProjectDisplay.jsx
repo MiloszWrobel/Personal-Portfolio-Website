99dcbc6251316f8cfe0b963dbf5afbc60c861a44
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2024-11-08 22:31:21 +0000

    Added more images and data to data.js and also added extra html elements in ProjectDisplay

--- src/components/ProjectDisplay.jsx
+++ src/components/ProjectDisplay.jsx
@@ -1,32 +1,81 @@
 import WEBSITES from "../data";
-import image1 from "../assets/city.png";
-import image2 from "../assets/city2.jpg";
-import image3 from "../assets/no-projects.png";
+import { SiGithub } from "react-icons/si";
+
+import { useState } from "react";
 export default function ProjectDisplay({ projectId, onReturn }) {
-  const images = [image1, image2, image3];
+  const [imageArray, setImageArray] = useState(
+    WEBSITES[projectId - 1].displayImages
+  );
+
+  function handleSwap(direction) {
+    setImageArray((prevArray) => {
+      if (direction === "left") {
+        return [prevArray[prevArray.length - 1], ...prevArray.slice(0, -1)];
+      } else {
+        return [...prevArray.slice(1), prevArray[0]];
+      }
+    });
+  }
+
   return (
-    <div className="min-h-screen bg-slate-300 relative ">
-      <h1 className=" absolute left-1/2 transform -translate-x-1/2 top-4 text-4xl text-stone-900 ">
+    <div className="min-h-screen bg-slate-300 relative  ">
+      <h1 className=" absolute left-1/2 transform -translate-x-1/2 top-4 text-6xl text-stone-900 ">
         {WEBSITES[projectId - 1].title}
       </h1>
       <button
         className=" absolute top-4 right-4 text-2xl text-stone-50 place-self-center justify-end bg-slate-800 rounded-xl p-2 hover:bg-slate-900 hover:text-stone-100 "
         onClick={onReturn}
       >
-        Back to menu
+        Back To Menu
       </button>
-      <div className="flex justify-center items-center pt-28 space-x-4">
-        <div className=" w-1/4 transform scale-75">
-          <img src={images[0]} className="object-cover w-full h-full" />
-        </div>
+      <div className="flex justify-center items-center pt-28 space-x-2">
+        <button
+          className=" w-1/4 transform scale-80 blur-sm hover:blur-none "
+          onClick={() => handleSwap("left")}
+        >
+          <img
+            src={imageArray[0]}
+            className="object-cover w-full h-[250px] max-w-full rounded-2xl"
+          />
+        </button>
 
         <div className=" w-1/2 transform scale-100">
-          <img src={images[1]} className="object-cover w-full h-full" />
+          <img
+            src={imageArray[1]}
+            className="object-cover w-full h-[600px] max-w-full border-slate-700 border-2"
+          />
         </div>
 
-        <div className=" w-1/4 transform scale-75">
-          <img src={images[2]} className="object-cover w-full h-full" />
-        </div>
+        <button
+          className=" w-1/4 transform scale-80 blur-sm hover:blur-none "
+          onClick={() => handleSwap("right")}
+        >
+          <img
+            src={imageArray[2]}
+            className="object-cover w-full h-[250px] max-w-full rounded-2xl"
+          />
+        </button>
+      </div>
+      <div className="pt-20 mx-20 ">
+        <h2 className="text-4xl font-semibold text-stone-900 border-b-4 border-stone-700 inline-block mb-4">
+          {WEBSITES[projectId - 1].title}
+        </h2>
+        <p className="text-lg text-stone-800 leading-relaxed mt-4 border-2 border-slate-700 p-4">
+          {WEBSITES[projectId - 1].text}
+        </p>
+        <button
+          onClick={() => handleLinkClick("https://github.com/MiloszWrobel")}
+          className="p-4 bg-slate-800 text-white rounded-full hover:bg-slate-900 mt-4 mb-4"
+        >
+          <SiGithub className="w-6 h-6 text-white " /> {/* GitHub Icon */}
+        </button>
+        <button
+          className=" absolute bottom-4 right-4 text-2xl text-stone-50 place-self-center justify-end bg-slate-800 rounded-xl p-2 hover:bg-slate-900 hover:text-stone-100 "
+          onClick={onReturn}
+        >
+          Back To Menu
+        </button>
+        <button></button>
       </div>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -1,8 +1,45 @@
-import noProjectsImage from "./assets/no-projects.png";
+import managmentLogo from "./assets/no-projects.png";
+import TTTLogo from "./assets/New-Logo.png";
+import calculatorLogo from "./assets/investment-calculator-logo.png";
+import portLogo from "./assets/portfolio-logo.jpg";
+import a1 from "./assets/a1.png";
+import a2 from "./assets/a2.png";
+import a3 from "./assets/a3.png";
+import a4 from "./assets/a4.png";
+import a5 from "./assets/a5.png";
+
+import quizLogo from "./assets/quiz-logo.png";
 
 export default [
-  { id: 1, image: noProjectsImage, title: "Tic-Tac-Toe" },
-  { id: 2, image: noProjectsImage, title: "Investment Calculator" },
-  { id: 3, image: noProjectsImage, title: "Project Managment" },
-  { id: 4, image: noProjectsImage, title: "Portfolio website" },
+  {
+    id: 1,
+    image: TTTLogo,
+    title: "Tic-Tac-Toe",
+    displayImages: [a1, a2, a3, a4, a5],
+    text: "This Tic Tac Toe website offers a versatile and engaging game experience with both player-versus-player and player-versus-AI game modes. Initially, this website originated from a React course that I've been actively studying. However, once I finished programming the website, I took it upon myself to continue working on it. Originally, the game was only playable with two players, and there was no AI mode. Naturally, I thought to myself that that was the next step I should take when working on a game such as this. Users can select their preferred mode from a Menu component, which provides options for the two-player mode or an AI opponent. For the AI games, players can choose from three difficulty levels: easy, medium, and hard, which adjust the AI's decision-making strategy. The hard AI difficulty is controlled using a separate Minimax component that enhances gameplay by employing strategic logic to create a challenging opponent. The easy difficulty simply selects a random available square, and the medium difficulty does the same unless it detects a winning move, which it will always take. The app is built with React functionalities such as state management (useState) for tracking game states, player names, and selected squares, creating a responsive and interactive experience. The TwoPlayer and AiGame components manage distinct game boards, each equipped with its own GameBoard component for rendering the grid and a GameOver component for displaying end-game messages. The app also includes a Log component to record each move, giving players a detailed history of their gameplay.",
+  },
+  {
+    id: 2,
+    image: calculatorLogo,
+    title: "Investment Calculator",
+    displayImages: [managmentLogo, TTTLogo, portLogo, a4, a5],
+  },
+  {
+    id: 3,
+    image: managmentLogo,
+    title: "Project Managment",
+    displayImages: [managmentLogo, TTTLogo, portLogo, a4, a5],
+  },
+  {
+    id: 4,
+    image: portLogo,
+    title: "Portfolio website",
+    displayImages: [managmentLogo, TTTLogo, portLogo, a4, a5],
+  },
+  {
+    id: 5,
+    image: quizLogo,
+    title: "Quiz Website",
+    displayImages: [managmentLogo, TTTLogo, portLogo, a4, a5],
+  },
 ];
